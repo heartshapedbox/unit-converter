fe1e250fefdfc462ebe5c940b80d5d8e936c9ee4
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-07-22 02:41:35 +0300

    Commit

--- converter.py
+++ converter.py
@@ -1,10 +1,10 @@
 from tkinter import *
-from tkinter import ttk
-from turtle import width
+from threading import Timer
 import customtkinter
-customtkinter.set_default_color_theme('dark-blue')
+import pyperclip
 import os
 os.chdir('C:\\Users\\baben\\Documents\\GitHub\\unit-converter\\')
+customtkinter.set_default_color_theme('dark-blue')
 
 
 root = customtkinter.CTk()
@@ -12,94 +12,159 @@ root.title('Unit Converter')
 x = int(root.winfo_screenwidth() // 2)
 y = int(root.winfo_screenheight() * 0.2)
 x, y = str(x), str(y)
-root.geometry(f'350x350+{x}+{y}')
+root.geometry(f'335x380+{x}+{y}')
 root.resizable(False, False)
 root.iconbitmap('assets\\logo.ico')
 
 
 class Converter():
     def __init__(self):
-        self.parentValue = StringVar()
+        self.parent_value = StringVar()
         self.parentsList = ['. . .','Length','Weight','Power']
-        self.childValue = StringVar()
-        self.classesList = ['. . .',KILO__METER(),METER__KILO(),KILO__MILE(),MILE__KILO(),KILO__GRAM(),GRAM__KILO(),KILO__POUND(),POUND__KILO(),WATT__HP(),HP__WATT(),WATT__JOULEHOUR(),JOULEHOUR__WATT()]
+        self.child_value1 = StringVar()
+        self.child_value2 = StringVar()
+        self.classes_list = ['. . .',KILO__METER(),METER__KILO(),KILO__MILE(),MILE__KILO(),MILE__METER(),METER__MILE(),KILO__GRAM(),GRAM__KILO(),KILO__POUND(),POUND__KILO(),POUND__GRAM(),GRAM__POUND(),WATT__HP(),HP__WATT(),WATT__JOULEHOUR(),JOULEHOUR__WATT(),JOULEHOUR__HP(),HP__JOULEHOUR()]
         self.dict = {}
-        self.showParents()
+        self.show_parents()
 
 
-    def showParents(self):
-        for i in range(0, 5):
-            for y in range(0, 2):
-                self.frame = Frame(root, borderwidth = 1)
-                self.frame.grid(row = 0, column = i, padx = 65)
-        self.parentLabel = customtkinter.CTkLabel(root, text = 'Choose a category')
-        self.parentLabel.grid(row = 0, column = 0, padx = 10, pady = 30, sticky='e')
-        self.parentValue.set(self.parentsList[0])
-        self.parentOption = customtkinter.CTkComboBox(root, variable=f"{self.parentValue}", values=[*self.parentsList], command = self.changeParent)
-        self.parentOption.grid(row = 0, column = 1, padx = 25, pady = 10, sticky='w')
+    def show_parents(self):
+        self.parent_label = customtkinter.CTkLabel(root, text = 'Category')
+        self.parent_label.grid(row = 0, column = 0, pady = 20, sticky='e')
+        self.parent_value.set(self.parentsList[0])
+        self.parent_option = customtkinter.CTkComboBox(root, variable=f"{self.parent_value}", values=[*self.parentsList], command = self.change_parent)
+        self.parent_option.grid(row = 0, column = 1, pady = 10, sticky='w')
+        self.parent_option.configure(width = 160)
 
-    def changeParent(self, *args):
-        coverLayer = customtkinter.CTkLabel(root, text = '')
-        coverLayer.place(width = 350, height = 250, y = 60)
-        self.childrenList = []
-        for i in range(1, len(self.classesList)):
-            if self.parentValue.get() == self.classesList[i].cat:
-                self.childrenList.append(self.classesList[i].child)
-                self.dict[self.classesList[i].child] = self.classesList[i].multiplier
-            elif self.parentValue.get() == '. . .':
+
+    def change_parent(self, *args):
+        self.cover_layer = customtkinter.CTkLabel(root, text = '')
+        self.cover_layer.place(width = 350, height = 500, y = 70)
+        self.children_list, self.children_list1, self.children_list2 = [], [], []
+        
+        for i in range(1, len(self.classes_list)):
+            if self.parent_value.get() == self.classes_list[i].cat:
+                self.children_list.append(self.classes_list[i].child)
+                self.dict[self.classes_list[i].child] = self.classes_list[i].multiplier
+                
+                if self.classes_list[i].child.split(' > ')[0] not in self.children_list1:
+                    self.children_list1.append(self.classes_list[i].child.split(' > ')[0])
+                if self.classes_list[i].child.split(' > ')[1] not in self.children_list2:
+                    self.children_list2.append(self.classes_list[i].child.split(' > ')[1])
+                    
+            elif self.parent_value.get() == '. . .':
                 quit()
-        self.showChildren()
+        self.show_children()
 
 
-    def showChildren(self):
-        self.childLabel = customtkinter.CTkLabel(root, text = 'Choose an option')
-        self.childLabel.grid(row = 1, column = 0, padx = 10, pady = 10, sticky='e')
-        self.childValue.set(self.childrenList[0])
-        self.childOption = customtkinter.CTkComboBox(root, variable=f"{self.childValue}", values = [*self.childrenList], command = self.changeChild)
-        self.childOption.grid(row = 1, column = 1, padx = 25, pady = 10, sticky='w')
-        self.entryLabel = customtkinter.CTkLabel(root, text = f'{self.childValue.get().split(" > ")[0]}')
-        self.entryLabel.grid(row = 2, column = 0, padx = 10, sticky='e')
-        self.entry = customtkinter.CTkEntry(root, width = 120)
-        self.entry.grid(row = 2, column = 1, padx = 25, pady = 10, sticky='w')
+    def show_children(self):
+        self.child_label1 = customtkinter.CTkLabel(root, text = 'From')
+        self.child_label1.grid(row = 1, column = 0, pady = 10, sticky='e')
+        self.child_value1.set(self.children_list[0].split(' > ')[0])
+        self.child_option1 = customtkinter.CTkComboBox(root, variable=f"{self.child_value1}", values = [*self.children_list1], command = self.change_child)
+        self.child_option1.grid(row = 1, column = 1, pady = 10, sticky='w')
+        self.child_option1.configure(width = 160)
+        
+        self.child_label2 = customtkinter.CTkLabel(root, text = 'To')
+        self.child_label2.grid(row = 2, column = 0, pady = 10, sticky='e')
+        self.child_value2.set(self.children_list[0].split(' > ')[1])
+        self.child_option2 = customtkinter.CTkComboBox(root, variable=f"{self.child_value2}", values = [*self.children_list2], command = self.change_child)
+        self.child_option2.grid(row = 2, column = 1, pady = 10, sticky='w')
+        self.child_option2.configure(width = 160)
+        
+        self.entry_label = customtkinter.CTkLabel(root, text = f'{self.child_value1.get()}', text_color = '#608bd5')
+        self.entry_label.grid(row = 3, column = 0, sticky='e')
+        self.entry = customtkinter.CTkEntry(root, width = 160)
+        self.entry.grid(row = 3, column = 1, pady = 10, sticky='w')
         self.entry.focus()
-        self.button = customtkinter.CTkButton(root, text = 'Convert', width = 120, height = 35, cursor = 'hand2', command = self.convert)
-        self.button.grid(row = 3, column = 1, padx = 25, pady = 10, sticky='w')
-        self.outputLabel = customtkinter.CTkLabel(root, text = f'{self.childValue.get().split(" > ")[1]}')
-        self.outputLabel.grid(row = 4, column = 0, padx = 10, sticky='e')
-        self.message = customtkinter.CTkLabel(root, text = '0', justify = LEFT)
-        self.message.grid(row = 4, column = 1, padx = 12, sticky='w')
+        self.convert_button = customtkinter.CTkButton(root, text = 'Convert', width = 160, height = 35, cursor = 'hand2', command = self.convert)
+        self.convert_button.grid(row = 4, column = 1, pady = 10, sticky='w')
+        self.convert_button.configure(hover_color = '#2e4f87')
+        self.switch_val = customtkinter.StringVar(value = "on")
+        self.switch = customtkinter.CTkSwitch(root, text = '', cursor = 'hand2', variable = self.switch_val, onvalue = "on", offvalue = "off", command = self.switch_child)
+        self.switch.grid(row = 4, column = 0, pady = 20, sticky='s')
+        self.switch.configure(fg_color = '#2e4f87')
+        self.output_label = customtkinter.CTkLabel(root, text = f'{self.child_value2.get()}', text_color = '#608bd5')
+        self.output_label.grid(row = 5, column = 0, sticky='e')
+        self.output = customtkinter.CTkLabel(root, text = '0', justify = LEFT)
+        self.output.grid(row = 5, column = 1, sticky='w')
+        self.copy_button = customtkinter.CTkButton(root, text = 'Copy', width = 70, height = 35, cursor = 'hand2', command = self.copy)
+        self.copy_button.grid(row = 6, column = 1, pady = 20, sticky='e')
+        self.copy_button.configure(hover_color = '#2e4f87')
+        self.reset_button = customtkinter.CTkButton(root, text = 'Reset', width = 70, height = 35, cursor = 'hand2', command = self.reset)
+        self.reset_button.grid(row = 6, column = 0, pady = 20, sticky='s')
+        self.reset_button.configure(fg_color = '#f757a4', hover_color = '#d6478d')
+        self.copy_message = customtkinter.CTkLabel(root, text = '', width = 1, justify = RIGHT)
+        self.copy_message.grid(row = 6, column = 1, padx = 20, pady = 20, sticky='w')
+        self.copy_message.configure(text_color = '#608bd5')   
+    
 
-
-    def cleanChild(self):
+    def clean_child(self):
         self.entry.delete(0, END)
-        self.message.configure(text = '0')
+        self.output.configure(text = '0')
 
 
-    def changeChild(self, args):
-        self.cleanChild()
-        for i in range(0, len(self.childrenList)):
-            if self.childValue.get() == self.childrenList[i]:
-                self.entryLabel.configure(text = self.childValue.get().split(" > ")[0])
-                self.outputLabel.configure(text = self.childValue.get().split(" > ")[1])
-            else:
-                self.cleanChild()
-
+    def change_child(self, args):
+        self.entry.configure(state = 'normal')
+        self.entry.focus()
+        self.copy_message.configure(text = '')
+        self.clean_child()
+        for i in range(0, len(self.children_list1)):
+            if self.children_list1[i] != self.children_list2[i]:
+                self.entry_label.configure(text = self.child_value1.get())
+                self.output_label.configure(text = self.child_value2.get())
+            elif self.child_value1.get() == self.child_value2.get():
+                self.entry.configure(state = 'disable')
+    
+    
+    def switch_child(self):
+        self.entry_label.configure(text = self.child_value2.get())
+        self.output_label.configure(text = self.child_value1.get())
+        i = self.child_option2.get()
+        y = self.child_option1.get()
+        self.child_option1.set(i)
+        self.child_option2.set(y)
+        
+    
 
     def convert(self):
-            for key in self.dict:
-                if len(self.entry.get()) > 14:
-                    self.cleanChild()
-                else:
-                    if key == self.childValue.get():
+        self.clip = ''
+        for key in self.dict:
+            if len(self.entry.get()) > 20:
+                self.clean_child()
+            else:
+                self.child_value = f'{self.child_value1.get()} > {self.child_value2.get()}'
+                if key == self.child_value:
+                    try:
+                        self.result = int(self.entry.get()) * self.dict[key]
+                        self.output.configure(text = str(round(self.result, 10)))
+                        self.clip = str(round(self.result, 10))
+                    except ValueError:
                         try:
-                            result = int(self.entry.get()) * self.dict[key]
-                            self.message.configure(text = str(round(result, 4)))
+                            self.result = float(self.entry.get()) * self.dict[key]
+                            self.output.configure(text = str(round(self.result, 10)))
+                            self.clip = str(round(self.result, 10))
                         except ValueError:
-                            try:
-                                result = float(self.entry.get()) * self.dict[key]
-                                self.message.configure(text = str(round(result, 4)))
-                            except ValueError:
-                                self.cleanChild()
+                            self.clean_child()
+                     
+    
+    def reset(self):
+        for i in (self.child_label1, self.child_label2, self.child_option1, self.child_option2, self.entry_label, self.entry, self.convert_button, self.output_label, self.output, self.copy_button, self.reset_button, self.copy_message, self.switch):
+            i.destroy()
+        converter = Converter()
+    
+    
+    def reset_copy_message(self):
+        self.copy_message.configure(text = '')
+    
+    
+    def copy(self):
+        pyperclip.copy(self.clip)
+        self.copy_message.configure(text = 'Copied!')
+        Timer(0.5, self.reset_copy_message).start()
+   
+
+
 
 
 class KILO__METER(Converter):
@@ -126,6 +191,21 @@ class MILE__KILO(Converter):
         self.child = 'Miles > Kilometers'
         self.multiplier = 1.6093
 
+class MILE__METER(Converter):
+    def __init__(self):
+        self.cat = 'Length'
+        self.child = 'Miles > Meters'
+        self.multiplier = 1609
+
+class METER__MILE(Converter):
+    def __init__(self):
+        self.cat = 'Length'
+        self.child = 'Meters > Miles'
+        self.multiplier = 0.0006214
+        
+        
+
+
 class KILO__GRAM(Converter):
     def __init__(self):
         self.cat = 'Weight'
@@ -150,6 +230,21 @@ class POUND__KILO(Converter):
         self.child = 'Pounds > Kilograms'
         self.multiplier = 0.4535
 
+class POUND__GRAM(Converter):
+    def __init__(self):
+        self.cat = 'Weight'
+        self.child = 'Pounds > Grams'
+        self.multiplier = 453.5
+        
+class GRAM__POUND(Converter):
+    def __init__(self):
+        self.cat = 'Weight'
+        self.child = 'Grams > Pounds'
+        self.multiplier = 0.002205
+
+
+
+
 class WATT__HP(Converter):
     def __init__(self):
         self.cat = 'Power'
@@ -173,6 +268,18 @@ class JOULEHOUR__WATT(Converter):
         self.cat = 'Power'
         self.child = 'Joule/Hour > Watt'
         self.multiplier = 0.000277
+
+class JOULEHOUR__HP(Converter):
+    def __init__(self):
+        self.cat = 'Power'
+        self.child = 'Joule/Hour > HP'
+        self.multiplier = 0.00000037
+
+class HP__JOULEHOUR(Converter):
+    def __init__(self):
+        self.cat = 'Power'
+        self.child = 'HP > Joule/Hour'
+        self.multiplier = 2684519.53
 
 if __name__ == '__main__':
     converter = Converter()
